5912b3970fa60a08ed18ad3aa6fb0f283a482cdb
Gustavo Coronel <gcoronelc@gmail.com>
2024-05-23 22:23:08 -0500

    Semana 1

---
+++ Semana01/ListaSimple/ListaSimple/ListaEnlazada.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSimple
+{
+    internal class ListaEnlazada
+    {
+        private Nodo cabeza;
+
+        public ListaEnlazada()
+        {
+            this.cabeza = null;
+        }
+
+        public void InsertarAlInicio(int dato)
+        {
+            Nodo nuevoNodo = new Nodo(dato);
+            nuevoNodo.siguiente = cabeza;
+            cabeza = nuevoNodo;
+        }
+
+        public void InsertarAlFinal(int dato)
+        {
+            Nodo nuevoNodo = new Nodo(dato);
+            if(cabeza == null)
+            {
+                cabeza = nuevoNodo;
+                return;
+            }
+            Nodo nodoAuxiliar = cabeza;
+            while(nodoAuxiliar.siguiente != null)
+            {
+                nodoAuxiliar = nodoAuxiliar.siguiente;
+            }
+            nodoAuxiliar.siguiente = nuevoNodo;
+        }
+
+        public void eliminarNodo(int dato)
+        {
+
+            if (cabeza == null)
+                return;
+
+            if (cabeza.dato == dato)
+            {
+                cabeza = cabeza.siguiente;
+                return;
+            }
+
+            Nodo anterior = null;
+            Nodo actual = cabeza;
+
+            while (actual != null && actual.dato != dato)
+            {
+                anterior = actual;
+                actual = actual.siguiente;
+            }
+
+            if (actual == null)
+                return;
+
+            anterior.siguiente = actual.siguiente;
+            actual.siguiente = null;
+
+        }
+
+        public void MostrarLista()
+        {
+            Nodo actual = cabeza;
+            while (actual != null)
+            {
+                Console.Write(actual.dato + " -> ");
+                actual = actual.siguiente;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
---
+++ Semana01/ListaSimple/ListaSimple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaSimple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListaEnlazada lista = new ListaEnlazada();
+
+            lista.InsertarAlInicio(3);
+            lista.InsertarAlInicio(9);
+            lista.InsertarAlInicio(5);
+            lista.InsertarAlInicio(7);
+            lista.InsertarAlInicio(9);
+
+            lista.InsertarAlFinal(55);
+
+            lista.eliminarNodo(3);
+
+            Console.WriteLine("Lista enlazada:");
+            lista.MostrarLista();
+
+            Console.ReadKey();
+        }
+    }
+}
