124de75ff3d4c3b08e14833e08c5ff39dbf31e25
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2024-05-28 18:40:27 -0500

    Semana 2

---
+++ Semana02/EjemploPila/EjemploPila/Pila.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPila
+{
+    internal class Pila
+    {
+
+        private Nodo tope;
+
+        public Pila()
+        {
+            tope = null;
+        }
+
+        public void Apilar(int valor)
+        {
+            Nodo nuevo = new Nodo(valor);
+            nuevo.Siguiente = tope;
+            tope = nuevo;
+        }
+
+        public int Desapilar()
+        {
+            if (tope == null)
+                throw new InvalidOperationException("La pila está vacía.");
+
+            int valor = tope.Valor;
+            tope = tope.Siguiente;
+            return valor;
+        }
+
+        public int Cima()
+        {
+            if (tope == null)
+                throw new InvalidOperationException("La pila está vacía.");
+
+            return tope.Valor;
+        }
+
+        public bool EstaVacia()
+        {
+            return tope == null;
+        }
+    }
+}
---
+++ Semana02/EjemploPila/EjemploPila/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pila pila = new Pila();
+
+            // Apilar elementos
+            pila.Apilar(1);
+            pila.Apilar(2);
+            pila.Apilar(3);
+
+            // Obtener el elemento en la cima de la pila
+            int cima = pila.Cima(); // Resultado: 3
+            Console.WriteLine(cima);
+
+            // Desapilar elementos
+            int desapilado = pila.Desapilar(); // Resultado: 3
+            Console.WriteLine(desapilado); // Salida: 3
+
+            // Verificar si la pila está vacía
+            bool estaVacia = pila.EstaVacia(); // Resultado: false
+            Console.WriteLine(estaVacia);
+
+            Console.ReadKey();
+
+        }
+    }
+}
