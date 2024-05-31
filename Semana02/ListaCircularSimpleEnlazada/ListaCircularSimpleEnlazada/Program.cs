17a917a5a98c8a6754fa085ee074d16e32fa6cd5
Gustavo Coronel <gcoronelc@gmail.com>
2024-05-30 22:20:34 -0500

    Semana 2

--- Semana02/ListaCircularSimpleEnlazada/ListaCircularSimpleEnlazada/Program.cs
+++ Semana02/ListaCircularSimpleEnlazada/ListaCircularSimpleEnlazada/Program.cs
@@ -13,19 +13,21 @@ namespace ListaCircularSimpleEnlazada
             CircularLinkedList list = new CircularLinkedList();
             list.AddFirst(5);
             list.AddFirst(4);
-            list.AddLast(6);
-            list.AddLast(7);
+            list.AddFirst(8);
+            //list.AddLast(6);
+            //list.AddLast(7);
 
             Console.WriteLine("Lista inicial:");
             list.PrintList();  // Output: Lista inicial: 4 5 6 7
 
+            /*
             list.Remove(5);
 
             Console.WriteLine("Lista después de eliminar 5:");
             list.PrintList();  // Output: Lista después de eliminar 5: 4 6 7
 
             Console.WriteLine("Número de nodos: " + list.Count);  // Output: Número de nodos: 3
-            
+            */
             Console.ReadKey();
         }
     }
